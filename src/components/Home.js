2d9df830eeb5ebe4e3cff6cd53287d54ebd9af87
Uzias Rivera <uziasrivera@gmail.com>
2020-08-31 18:07:42 -0700

    styled to support mobile and tablet

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="body-wrap">
                 <div className="body-text">
                     <img className="home-image" src={gym} ></img>
-                    <p className="body-title-text">Record all your workouts <br></br> Track your progression as precisely as possible</p>
+                    <p className="body-title-text">Record every workout <br></br> Track progression as precisely as possible</p>
                 </div>
                 <div className="body-image">
                     <img src={stats} className="phone-image" alt="phone demo" />
